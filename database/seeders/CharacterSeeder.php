b9f597428c6ed2686fa050dfb542680a1c19f8a6
Arwel Brown <arwel.brown@hotmail.co.uk>
2023-09-07 23:52:54 +0100

    quick changes to fix a few issues. added all seeders to gitignore

--- database/seeders/CharacterSeeder.php
+++ database/seeders/CharacterSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CharacterSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class CharacterSeeder extends Seeder
             [
                 ''
             ]
-        ])
+        ]);
     }
 }
