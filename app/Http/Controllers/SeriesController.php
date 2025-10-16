8e35fb0f0555f4dbda99667bdc044c78038f6435
Arwel Brown <arwel.brown@hotmail.co.uk>
2025-10-16 20:24:06 +0100

    removed unused import

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Series;
 
 class SeriesController extends Controller
 {
     public static function getSeries(int $id): object
     {
-        return Series::where('id', $id)->first();
+        return Series::where("id", $id)->first();
     }
 }
